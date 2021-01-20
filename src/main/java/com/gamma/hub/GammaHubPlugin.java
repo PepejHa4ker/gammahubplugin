aadf2ab92c6076cd9ddf0b228e96a39264fb01ca
pepej <semenovich-nikitka@mail.ru>
2021-01-21 02:50:46 +1000

    Initial commit

---
+++ src/main/java/com/gamma/hub/GammaHubPlugin.java
@@ -0,0 +1,8 @@
+package com.gamma.hub;
+
+import com.pepej.papi.ap.Plugin;
+import com.pepej.papi.ap.PluginDependency;
+
+@Plugin(name = "GammaHub", version = "1.0.0", depends = @PluginDependency("papi"))
+public class GammaHubPlugin {
+}
